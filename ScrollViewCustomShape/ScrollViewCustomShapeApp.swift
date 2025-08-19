9eb9d2be315e4ef8701e3f464ccdcaff2a0e8d41
itsuki <itsuki.enjyoy@gmail.com>
2025-08-19 12:00:16 +0900

    Initial Commit

---
+++ ScrollViewCustomShape/ScrollViewCustomShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewCustomShapeApp.swift
+//  ScrollViewCustomShape
+//
+//  Created by Itsuki on 2025/08/19.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewCustomShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
